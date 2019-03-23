42ccd9e3105aacdeec3ff61f883b6712ae723c83
GregLacour <lacour.gregory@elypsiamusicgroup.com>
2019-03-23 19:20:58 +0100

    Update discogsmaster
    Added new fields available online.

--- DiscogsClient/Data/Result/DiscogsMaster.cs
+++ DiscogsClient/Data/Result/DiscogsMaster.cs
@@ -4,6 +4,10 @@
     {
         public int main_release { get; set; }
         public string main_release_url { get; set; }
+        public int most_recent_release { get; set; }
+        public string most_recent_release_url { get; set; }
+        public int num_for_sale { get; set; }
+        public decimal lowest_price { get; set; }
         public string versions_url { get; set; }
     }
 }
